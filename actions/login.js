9a66a0c583c840f756aa37a139762e2e836155d8
ravi18 <ravi@arrowai.com>
2024-05-29 13:26:29 +0530

    login logout added

---
+++ actions/login.js
@@ -0,0 +1,65 @@
+const { v4: uuidv4 } = require('uuid');
+const axios = require('axios')
+const fs = require('fs');
+const usersFile = 'users.json';
+
+// Function to delete the users.json file
+function deleteUserFile() {
+    try {
+        fs.unlinkSync(usersFile);
+        // console.log('users.json file deleted successfully');
+    } catch (err) {
+        // console.error('Error deleting users.json file:', err);
+    }
+}
+
+// Function to log out the user
+const logout = () => {
+    deleteUserFile();
+    console.log('Logged out successfully');
+};
+
+
+const sinIn = () => {
+    const uuid = uuidv4();
+
+    // Simulating window.open in a Node.js environment
+    console.log(`Please open this URL to login: http://portal.codebolt.ai/performSignIn?uid=${uuid}&loginflow=app`);
+    const intervalId = setInterval(async () => {
+      try {
+        const response = await axios.get(
+          `https://us-central1-codeboltai.cloudfunctions.net/checkOneTimeToken?oneTimeToken=${uuid}`
+        );
+        // console.log(response.data)
+        if (response.status === 200) {
+          clearInterval(intervalId);
+          console.log('Login successful!');
+          saveUserApiResponse(response.data); 
+        }
+       // Assuming there is a function saveUserApiResponse to handle saving the response
+      } catch (error) {
+        // console.error('Error checking token:', error);
+      }
+    }, 1000);
+  };
+
+
+// Function to read user data from the file
+function readUsers() {
+    try {
+        const data = fs.readFileSync(usersFile, 'utf8');
+        return JSON.parse(data);
+    } catch (err) {
+        return [];
+    }
+}
+
+// Function to write user data to the file
+function saveUserApiResponse(user) {
+    fs.writeFileSync(usersFile, JSON.stringify(user, null, 4));
+}
+
+module.exports={
+    sinIn,
+    logout
+}
--- codebolt.js
+++ codebolt.js
@@ -1,18 +1,36 @@
 #!/usr/bin/env node
 
+
+
 const { program } = require('commander');
 const { getVersion } = require('./actions/version');
 const { uploadFolder } = require('./actions/uploadfolder');
+const inquirer = require('inquirer');
+
+const {sinIn,logout} = require('./actions/login')
+
 
 program.version('1.0.0');
-program
-  .command('version')
-  .description('Check the application version')
-  .action(getVersion);
 
 program
-  .command('upload <folderPath>')
-  .description('Upload a folder')
-  .action(uploadFolder);
+    .command('version')
+    .description('Check the application version')
+    .action(getVersion);
+
+program
+    .command('login')
+    .description('Log in to the application')
+    .action(sinIn);
+
+program
+    .command('logout')
+    .description('Log out of the application') // Added for logout
+    .action(logout); // Added for logout
+
+program
+    .command('upload <folderPath>')
+    .description('Upload a folder')
+    .action(uploadFolder);
 
 program.parse(process.argv);
+
