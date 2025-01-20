8e567faac441151c25059cd1437d8afff51a3102
Ravi <ravi@arrowai.com>
2025-01-20 12:02:14 +0530

    waitfor connection removed

--- template/basic/index.js
+++ template/basic/index.js
@@ -2,7 +2,7 @@ const codebolt = require('@codebolt/codeboltjs').default;
 
 codebolt.chat.onActionMessage().on("userMessage", async (req, response) => {
 
-    await codebolt.waitForConnection();
+
  
 })
 
