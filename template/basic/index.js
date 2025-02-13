f8da9e0bfb9acb55b40624b5e03f5fcf7b508900
Ravi <ravi@arrowai.com>
2025-02-13 17:48:51 +0530

    changes

--- template/basic/index.js
+++ template/basic/index.js
@@ -1,8 +1,8 @@
 const codebolt = require('@codebolt/codeboltjs').default;
 
 codebolt.chat.onActionMessage().on("userMessage", async (req, response) => {
-
-
+     codebolt.chat.sendMessage("Hello from Codebolt");
+     response("done")
  
 })
 
