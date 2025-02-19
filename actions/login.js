2d57d490e2a05a267c68998cdb89c16f2c1ee581
Nursid Ansari <114871863+Nursid@users.noreply.github.com>
2025-02-19 15:11:16 +0530

    updated getSigninAPi

--- actions/login.js
+++ actions/login.js
@@ -1,11 +1,7 @@
 const { v4: uuidv4 } = require('uuid');
-const axios = require('axios')
-const fs = require('fs');
+const axios = require('axios');
 const chalk = require('chalk');
-const { saveUserData, getUserData, checkUserAuth, deleteUserData } = require('./userData');
-
-// Function to delete the users.json file
-
+const { saveUserData, checkUserAuth, deleteUserData } = require('./userData');
 
 // Function to log out the user
 const logout = () => {
@@ -13,38 +9,34 @@ const logout = () => {
     console.log('Logged out successfully');
 };
 
-
+// Function to sign in the user
 const signIn = () => {
     if (checkUserAuth()) {
         console.log(chalk.yellow('Already logged in.'));
     } else {
         const uuid = uuidv4();
-       
         console.log(chalk.blue(`Please open this URL to login: http://portal.codebolt.ai/performSignIn?uid=${uuid}&loginflow=app`));
-        
-        const intervalId = setInterval(async () => {
-          try {
-            // const response = await axios.get(
-            //   `https://us-central1-codeboltai.cloudfunctions.net/checkOneTimeToken?oneTimeToken=${uuid}`
-            // );
-            const response = await axios.get(
-              `https://api.codebolt.ai/api/auth/addonetimetoken?oneTimeToken=${uuid}`
-            );
 
-            if (response.status === 200) {
-              clearInterval(intervalId);
-              console.log(chalk.green('Login successful!'));
-              saveUserData(response.data);
+        const intervalId = setInterval(async () => {
+            try {
+              console.log(`https://api.codebolt.ai/api/auth/addonetimetoken?oneTimeToken=${uuid}`);
+                const response = await axios.post(
+                    `https://api.codebolt.ai/api/auth/addonetimetoken?oneTimeToken=${uuid}`
+                );
+
+                if (response.status === 200) {
+                    clearInterval(intervalId);
+                    console.log(chalk.green('Login successful!'));
+                    saveUserData(response.data);
+                }
+            } catch (error) {
+                console.error(chalk.red('Error checking token:', error));
             }
-           // Assuming there is a function saveUserApiResponse to handle saving the response
-          } catch (error) {
-            // console.log('Error checking token:', error);
-          }
         }, 1000);
     }
 };
 
-module.exports={
+module.exports = {
     signIn,
     logout
-}
+};
