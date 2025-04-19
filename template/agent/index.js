3a4816f089d2c7abe2c7ea55610da2f67c6df243
Ravi <ravi@arrowai.com>
2025-04-19 14:21:45 +0530

    changes

--- template/agent/index.js
+++ template/agent/index.js
@@ -7,9 +7,9 @@ codebolt.chat.onActionMessage().on("userMessage", async (req, response) => {
     try {
         const userMessage = new UserMessage(req.message);
         const systemPrompt = new SystemPrompt("./agent.yaml", "test");
-        const agentTools = await codebolt.tools.getAvailableToolBoxes("codebolt");
+        const agentTools = await codebolt.tools.listToolsFromToolBoxes(["codebolt"]);
         const task = new TaskInstruction(agentTools, userMessage, "./task.yaml", "main_task");
-        const agent = new Agent([], systemPrompt);
+        const agent = new Agent(agentTools, systemPrompt);
         const {message, success, error } = await agent.run(task);
         response(message ? message : error);
 
